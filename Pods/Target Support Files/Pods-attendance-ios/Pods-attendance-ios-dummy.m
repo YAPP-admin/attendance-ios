927311838af5d63bef98bca1671327eaff30099e
sanghee-dev <leeesangheee@gmail.com>
2022-01-31 18:29:02 +0900

    초기 세팅

---
+++ Pods/Target Support Files/Pods-attendance-ios/Pods-attendance-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_attendance_ios : NSObject
+@end
+@implementation PodsDummy_Pods_attendance_ios
+@end
---
+++ attendance-ios/Source/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  attendance-ios
+//
+//  Created by leeesangheee on 2022/01/31.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
