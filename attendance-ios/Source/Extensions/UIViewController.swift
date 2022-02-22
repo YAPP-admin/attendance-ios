5214d07572c446af5405c308a5c8a3c3b6b4696d
BOMS2 <kbm97121@swu.ac.kr>
2022-02-22 23:07:44 +0900

    font 경로 수정

--- attendance-ios/Source/Extensions/UIViewController.swift
+++ attendance-ios/Source/Extensions/UIViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 extension UIViewController {
     func showToast(message: String) {
-        let toastLabel = UILabelWithRound(frame: .zero, color: UIColor.clear.cgColor, width: 0, inset: UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
-        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        let toastLabel = UILabelWithRound(frame: .zero, color: UIColor.clear.cgColor, width: 0, inset: UIEdgeInsets(top: 12, left: 24, bottom: 12, right: 24))
+        toastLabel.backgroundColor = UIColor(red: 0.173, green: 0.185, blue: 0.208, alpha: 0.8)
         toastLabel.textColor = .white
         toastLabel.textAlignment = .center
         toastLabel.text = message
-        toastLabel.alpha = 1.0
+		toastLabel.font(.Body2)
         toastLabel.numberOfLines = 0
         self.view.addSubview(toastLabel)
         toastLabel.snp.makeConstraints {
-            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(20)
-            $0.leading.greaterThanOrEqualToSuperview().inset(16)
-            $0.trailing.lessThanOrEqualToSuperview().inset(16)
-            $0.width.greaterThanOrEqualTo(20)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(84)
+            $0.leading.greaterThanOrEqualToSuperview().inset(24)
+            $0.trailing.lessThanOrEqualToSuperview().inset(24)
+            $0.width.greaterThanOrEqualTo(44)
             $0.centerX.equalToSuperview()
         }
-        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: 5.0, delay: 0.1, options: .curveEaseIn, animations: {
             toastLabel.alpha = 0.0
         }, completion: { _ in
             toastLabel.removeFromSuperview()
