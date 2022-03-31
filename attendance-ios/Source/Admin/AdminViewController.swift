50b771341a30a73f78d895b92f95c13fa2a8d21b
sanghee-dev <sangheee.lee@navercorp.com>
2022-03-31 21:41:31 +0900

    #10 출결관리 기본 UI 틀 작성

--- attendance-ios/Source/Admin/AdminViewController.swift
+++ attendance-ios/Source/Admin/AdminViewController.swift
@@ -13,10 +13,38 @@ import UIKit
 final class AdminViewController: UIViewController {
 
     enum Constants {
-        static let padding: CGFloat = 24
+        static let horizontalPadding: CGFloat = 24
+        static let verticalPadding: CGFloat = 28
+        static let dividerViewHeight: CGFloat = 12
+        static let todayViewHeight: CGFloat = 65
     }
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "출결 관리"
+        label.font = .Pretendard(type: .Bold, size: 24)
+        label.textColor = .gray_1200
+        label.numberOfLines = 0
+        return label
+    }()
+
+    // TODO: - 배경색 Light/Background_base로 변경 필요
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray_200
+        return view
+    }()
     private let cardView = AdminCardView()
+    private let todayView = AdminTodayView()
+
+    private let sessionCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = .gray_200
+        return collectionView
+    }()
 
     private let viewModel = AdminViewModel()
     private var disposeBag = DisposeBag()
@@ -34,11 +62,7 @@ final class AdminViewController: UIViewController {
 private extension AdminViewController {
 
     func bindViewModel() {
-//        cardButton.rx.tap
-//            .bind { [weak self] _ in
-//                print("누적 점수 확인하기")
-//            }
-//            .disposed(by: disposeBag)
+
     }
 
     func setupDelegate() {
@@ -51,11 +75,34 @@ private extension AdminViewController {
 
     func configureLayout() {
         view.addSubview(cardView)
+        view.addSubview(dividerView)
+        view.addSubview(titleLabel)
+        view.addSubview(todayView)
+        view.addSubview(sessionCollectionView)
 
         cardView.snp.makeConstraints {
             $0.top.equalToSuperview().offset(105)
-            $0.left.right.equalToSuperview().inset(Constants.padding)
+            $0.left.right.equalToSuperview().inset(Constants.horizontalPadding)
             $0.height.equalTo(100)
+        }
+        dividerView.snp.makeConstraints {
+            $0.top.equalTo(cardView.snp.bottom).offset(Constants.verticalPadding)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(Constants.dividerViewHeight)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(dividerView.snp.bottom).offset(Constants.verticalPadding)
+            $0.left.right.equalToSuperview().inset(Constants.horizontalPadding)
+        }
+        todayView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(Constants.verticalPadding)
+            $0.left.right.equalToSuperview().inset(Constants.horizontalPadding)
+            $0.height.equalTo(Constants.todayViewHeight)
+        }
+        sessionCollectionView.snp.makeConstraints {
+            $0.top.equalTo(todayView.snp.bottom).offset(Constants.verticalPadding)
+            $0.left.right.equalToSuperview().inset(Constants.horizontalPadding)
+            $0.bottom.equalToSuperview().inset(48)
         }
     }
 
