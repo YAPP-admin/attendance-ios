81a3b02676835f81de9b55ec9ec5062daaf73af1
sanghee-dev <sangheee.lee@navercorp.com>
2022-04-06 22:16:33 +0900

    #10 세션 셀 UI

--- attendance-ios/Source/Admin/AdminSessionCell.swift
+++ attendance-ios/Source/Admin/AdminSessionCell.swift
@@ -11,8 +11,29 @@ import UIKit
 final class AdminSessionCell: UICollectionViewCell {
 
     enum Constants {
-        static let padding: CGFloat = 8
+        static let horizontalPadding: CGFloat = 24
     }
+
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .Pretendard(type: .regular, size: 16)
+        label.textColor = .gray_1200
+        return label
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray_1200
+        label.font = .Pretendard(type: .semiBold, size: 16)
+        return label
+    }()
+
+    private let arrowButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.tintColor = .gray_600
+        return button
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,7 +55,21 @@ private extension AdminSessionCell {
     }
 
     func configureLayout() {
+        addSubviews([dateLabel, titleLabel, arrowButton])
 
+        dateLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(Constants.horizontalPadding)
+            $0.centerY.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints {
+            $0.left.equalTo(dateLabel).inset(24)
+            $0.centerY.equalToSuperview()
+        }
+        arrowButton.snp.makeConstraints {
+            $0.right.equalToSuperview().inset(Constants.horizontalPadding)
+            $0.width.height.equalTo(12)
+            $0.centerY.equalToSuperview()
+        }
     }
 
 }
