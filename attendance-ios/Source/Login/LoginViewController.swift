c1325858c23aaa33c91a87cb4a529a295cf25511
leeesangheee <sangheee.lee@navercorp.com>
2022-04-15 02:06:27 +0900

    #45 애플 로그인 버튼 생성

--- attendance-ios/Source/Login/LoginViewController.swift
+++ attendance-ios/Source/Login/LoginViewController.swift
@@ -14,7 +14,10 @@ final class LoginViewController: UIViewController {
 
     enum Constants {
         static let padding: CGFloat = 24
-        static let buttonSpacing: CGFloat = 6
+        static let buttonPadding: CGFloat = 6
+        static let buttonSpacing: CGFloat = 8
+        static let buttonHeight: CGFloat = 44
+        static let buttonBottomSpacing: CGFloat = 133
         static let cornerRadius: CGFloat = 12
         static let kakaoBlack: UIColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
     }
@@ -39,17 +42,31 @@ final class LoginViewController: UIViewController {
         return label
     }()
 
-    private let loginButton: UIButton = {
+    private let appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Apple로 로그인", for: .normal)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .Pretendard(type: .regular, size: 19)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setImage(UIImage(systemName: "applelogo"), for: .normal) // 이미지 수정
+        button.titleEdgeInsets = .init(top: 0, left: Constants.buttonPadding/2, bottom: 0, right: -Constants.buttonPadding/2)
+        button.imageEdgeInsets = .init(top: 0, left: -Constants.buttonPadding/2, bottom: 0, right: Constants.buttonPadding/2)
+        button.layer.cornerRadius = Constants.cornerRadius
+        return button
+    }()
+
+    private let kakaoLoginButton: UIButton = {
         let button = UIButton()
         button.setTitle("카카오 로그인", for: .normal)
         button.backgroundColor = .yapp_kakao_yellow
-        button.titleLabel?.font = .Pretendard(type: .medium, size: 19)
-        button.setTitleColor(Constants.kakaoBlack, for: .normal)
-        button.setImage(UIImage(systemName: "bubble.left.fill"), for: .normal)
-        button.titleEdgeInsets = .init(top: 0, left: Constants.buttonSpacing/2, bottom: 0, right: -Constants.buttonSpacing/2)
-        button.imageEdgeInsets = .init(top: 0, left: -Constants.buttonSpacing/2, bottom: 0, right: Constants.buttonSpacing/2)
-        button.backgroundColor = .yapp_kakao_yellow
         button.tintColor = Constants.kakaoBlack
+        button.setTitleColor(Constants.kakaoBlack, for: .normal)
+        button.titleLabel?.font = .Pretendard(type: .regular, size: 19)
+        button.setImage(UIImage(named: "kakao_login"), for: .normal)
+        button.titleEdgeInsets = .init(top: 0, left: Constants.buttonPadding/2, bottom: 0, right: -Constants.buttonPadding/2)
+        button.imageEdgeInsets = .init(top: 0, left: -Constants.buttonPadding/2, bottom: 0, right: Constants.buttonPadding/2)
         button.layer.cornerRadius = Constants.cornerRadius
         return button
     }()
@@ -91,7 +108,7 @@ extension LoginViewController: WKNavigationDelegate {
 private extension LoginViewController {
 
     func bindSubviews() {
-        loginButton.rx.tap
+        kakaoLoginButton.rx.tap
             .throttle(.seconds(3), latest: false, scheduler: MainScheduler.instance)
             .bind(to: viewModel.input.tapLogin)
             .disposed(by: disposeBag)
@@ -176,25 +193,30 @@ private extension LoginViewController {
     }
 
     func configureLayout() {
-        view.addSubviews([titleLabel, webView, loginButton, splashView, secretAdminButton])
+        view.addSubviews([titleLabel, webView, appleLoginButton, kakaoLoginButton, splashView, secretAdminButton])
 
         webView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(80)
+            $0.top.equalToSuperview().offset(40)
             $0.left.right.equalToSuperview().inset(68)
             $0.height.equalTo(view.bounds.width)
         }
         titleLabel.snp.makeConstraints {
-            $0.bottom.equalToSuperview().inset(180)
+            $0.top.equalTo(webView.snp.bottom)
             $0.left.right.equalToSuperview().inset(Constants.padding)
         }
-        loginButton.snp.makeConstraints {
-            $0.bottom.equalToSuperview().inset(57)
+        appleLoginButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(Constants.buttonBottomSpacing)
             $0.left.right.equalToSuperview().inset(Constants.padding)
-            $0.height.equalTo(45)
+            $0.height.equalTo(Constants.buttonHeight)
+        }
+        kakaoLoginButton.snp.makeConstraints {
+            $0.top.equalTo(appleLoginButton.snp.bottom).offset(Constants.buttonSpacing)
+            $0.left.right.equalToSuperview().inset(Constants.padding)
+            $0.height.equalTo(Constants.buttonHeight)
         }
         splashView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(40)
+            $0.top.equalToSuperview().offset(40)
+            $0.bottom.equalToSuperview().inset(80)
             $0.left.right.equalToSuperview().inset(10)
         }
         secretAdminButton.snp.makeConstraints {
